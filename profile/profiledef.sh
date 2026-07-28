f498bda83d8d0e1d0dac4b5ee8cb9db7b30780fb
AxonShell <kunilarch@gmail.com>
2026-07-28 17:42:50 +0200

    fixed wallpaper integration

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -45,6 +45,7 @@ file_permissions=(
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
-
+  ["/usr/local/bin/mupdate"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/share/backgrounds/mulios"]="0:0:755"
 )
