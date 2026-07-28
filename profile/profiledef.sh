5110294b122dc1177fad9bcb5a31561f605acf50
AxonShell <kunilarch@gmail.com>
2026-07-28 15:24:30 +0200

    Customize ArchISO profile for MuliOS

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -1,26 +1,51 @@
-# MuliOS Developpement Team
-#
-# Notice To Developers. iso_label and iso_version "_version" is a placeholder. replace with your own version's name
-# make sure it is easy to remember, and is based of the version you are using.
-#
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
 
 iso_name="MuliOS"
-iso_label="MuliOS_version"
-iso_publisher="MuliOS"
-iso_application="MuliOS Arch Linux"
-iso_version="version"
+iso_label="MULIOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="MuliOS Project"
+iso_application="MuliOS Arch Linux Live ISO"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+
 install_dir="arch"
+
 buildmodes=('iso')
 
 bootmodes=(
-  'uefi-x64.systemd-boot.esp-mode'
-  'bios.syslinux.mbr'
-  'bios.syslinux.eltorito'
+  'bios.syslinux'
+  'uefi.systemd-boot'
 )
 
-arch="x86_64"
 pacman_conf="pacman.conf"
+
 airootfs_image_type="squashfs"
+
+airootfs_image_tool_options=(
+  '-comp' 'xz'
+  '-Xbcj' 'x86'
+  '-b' '1M'
+  '-Xdict-size' '1M'
+)
+
+bootstrap_tarball_compression=(
+  'zstd'
+  '-c'
+  '-T0'
+  '--auto-threads=logical'
+  '--long'
+  '-19'
+)
+
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
+
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+
+  ["/opt/mulios"]="0:0:755"
+  ["/usr/share/backgrounds/mulios"]="0:0:755"
+  ["/usr/share/fastfetch"]="0:0:755"
 )
