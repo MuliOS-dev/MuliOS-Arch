df95a9564b2a0fa14bd2efea75a85f87a8028ef3
Axon <kunilarch@gmail.com>
2026-09-26 17:21:11 +0200

    Installer: remove unused desktop environment choices

--- profile/airootfs/opt/mulios-installer/pages/desktop_page.py
+++ profile/airootfs/opt/mulios-installer/pages/desktop_page.py
@@ -1,7 +1,4 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QLabel, QButtonGroup
-
-from widgets.select_card import SelectCard
-from config.settings import DESKTOP_ENVIRONMENTS
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
 
 
 class DesktopPage(QWidget):
@@ -15,34 +12,18 @@ class DesktopPage(QWidget):
         title.setObjectName("TitleLabel")
         layout.addWidget(title)
 
-        subtitle = QLabel("Choose what your MuliOS desktop looks like.")
+        subtitle = QLabel(
+            "MuliOS uses KDE Plasma as its desktop environment."
+        )
         subtitle.setObjectName("SubtitleLabel")
+        subtitle.setWordWrap(True)
         layout.addWidget(subtitle)
 
-        layout.addSpacing(8)
+        card = QLabel("KDE Plasma")
+        card.setObjectName("FixedChoiceCard")
+        layout.addWidget(card)
 
-        grid = QGridLayout()
-        grid.setSpacing(12)
-        self.button_group = QButtonGroup(self)
-        self.button_group.setExclusive(True)
-        self._value_by_button = {}
-
-        for i, (value, label) in enumerate(DESKTOP_ENVIRONMENTS):
-            card = SelectCard("", label, object_name="SelectCard")
-            row, col = divmod(i, 2)
-            grid.addWidget(card, row, col)
-            self.button_group.addButton(card)
-            self._value_by_button[card] = value
-
-        layout.addLayout(grid)
         layout.addStretch()
 
-        # Default to first entry (Xfce, MuliOS default)
-        first_button = list(self._value_by_button.keys())[0]
-        first_button.setChecked(True)
-
     def selected_desktop(self):
-        checked = self.button_group.checkedButton()
-        if checked is None:
-            return DESKTOP_ENVIRONMENTS[0][0]
-        return self._value_by_button[checked]
+        return "kde"
